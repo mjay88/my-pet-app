49212a9aa47c0cb014ca41f8e200bc605390bcc5
Michael W Jarrett <67067893+mjay88@users.noreply.github.com>
2022-06-26 12:47:06 -0500

    added styling

--- client/src/components/AnimalCard.js
+++ client/src/components/AnimalCard.js
@@ -6,23 +6,24 @@ const AnimalCard = (props) => {
 
   return (
     <div className="animal-card">
-      <div className="animal-card__inner">
-      <div className="animal__image" component="img">
-        {animal.photos && animal.photos[0] && animal.photos[0].full ? (
-          <img src={animal.photos[0].full} alt={"adoptable animal"} />
-        ) : (
-          <div>
-            <span>No Photo Found</span>
-          </div>
-        )}
-      </div>
-      <h1 className="animal-card__header">{animal.name}</h1>
+      {animal.photos && animal.photos[0] && animal.photos[0].full ? (
+        <img  src={animal.photos[0].full} alt={"adoptable animal"} className="animal-card__image" />
+      ) : (
+        <div>
+          <span>No Photo Found</span>
+        </div>
+      )}
       <div className="animal-card__content">
+        <h1 className="animal-card__header">{animal.name}</h1>
         <p>{animal.species}</p>
         <p>{animal.age}</p>
         <p>{animal.breeds.primary}</p>
-        <p></p>
-      </div>
+        <div className="animal-card__inner-content">
+         
+        <button className="ripple btn1">Like</button>
+        <button className="ripple btn2">More Info!</button> 
+
+        </div>
       </div>
     </div>
   );
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import Search from "./Search";
 import AuthBox from "./AuthBox";
+import Footer from "./Footer";
+import Home from "./Home";
 import "../App.scss";
 
 const Layout = (props) => {
@@ -13,16 +15,15 @@ const {animals} = props;
     <BrowserRouter>
        <Header />
       <Routes>
-        <Route exact path="/" element={<h1>Home</h1>} />
+        <Route exact path="/" element={<Home/>} />
         <Route exact path="/search" element={<Search animals={animals}/>} />
         {/**if registration prop is true, conditional logic for login router, else -  route */}
         <Route exact path="/favorites" element={<h1>Favorites</h1>} />
         <Route exact path="/register" element={<AuthBox register/>} />
         <Route exact path="/login" element={<AuthBox />} />
 
-        
-
       </Routes>
+        <Footer />
     </BrowserRouter>
   );
 };
